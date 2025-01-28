1c1efb7d0b96545c65382f63ef210647d6a8bec6
Roman Tokarskiy <rtokarskij111@gmail.com>
2025-01-28 22:02:18 +1000

    1

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About() {
 	return (
-		<section>
+		<section className='mx-2 md:mx-0'>
 			<h6 className='text-right mr-3 mt-2 mb-5 text-sm text-gray-400'>+123456789, "ОАО Тигран"</h6>
 			<h2 className='text-center my-5 font-medium text-3xl'>Заголовок</h2>
 			<p className='adaptive-p'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut possimus modi optio totam ipsam natus, obcaecati est voluptates accusamus provident atque repellendus cupiditate quae, reiciendis velit. Reiciendis, id accusamus eius veritatis dolor sequi quidem nostrum? Tempora expedita eos ipsa obcaecati! Molestias sed veritatis reprehenderit ab quia totam ullam odit fugiat dolorem asperiores! Sequi minus deserunt aperiam et omnis est doloremque corporis, iusto tenetur tempora, fugiat porro? Facere, excepturi incidunt? Dolore impedit sint recusandae sequi repudiandae? Corporis fugit ut possimus dignissimos, sapiente laboriosam maiores mollitia voluptates natus nulla id modi eaque facilis ipsa, maxime pariatur eum ipsam voluptatum odio laudantium quae corrupti, cupiditate dolore suscipit. Neque expedita deserunt necessitatibus exercitationem dicta! Cumque repellat sequi, minima ipsam iusto voluptas odio voluptates earum corrupti eveniet at incidunt amet distinctio natus nulla repudiandae illo consequatur harum! Nulla laborum soluta quo facilis, voluptatem quibusdam consectetur aperiam at omnis. Natus blanditiis corporis assumenda consequatur quis aperiam.</p>
